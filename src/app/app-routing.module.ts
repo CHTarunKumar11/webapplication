3727635c1ce7c7caff5b59f2f3f5502eb99e83a3
tarun <tarunkumarchintala@gmail.com>
2021-04-15 14:13:14 +0530

    16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotpwdComponent } from './forgotpwd/forgotpwd.component';
+import { FormsComponent } from './forms/forms.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"/forms/login",pathMatch:"full"},
+  {path:"forms",redirectTo:"/forms/login",pathMatch:"full"},
+  {path:"forms",component:FormsComponent,children:[
+    {path:"login",component:LoginComponent},
+    {path:"register",component:RegisterComponent},
+    {path:"forgotpwd",component:ForgotpwdComponent},
+  ]},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ForgotpwdComponent } from './forgotpwd/forgotpwd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForgotpwdComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
