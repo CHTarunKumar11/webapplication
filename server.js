a3708cebf601d6d1ef26d4325a37fd53039055e5
tarun <tarunkumarchintala@gmail.com>
2021-04-15 11:36:56 +0530

    third

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const exp = require("express");
 const app = exp();
-const mongoose = require("mongoose");
+const mongodb = require("mongodb").MongoClient;
 const path = require("path");
-
-app.use(exp.static(path.join(__dirname,"./dist/webapp")));
-
-app.use(exp.json());
 
 require("dotenv").config();
 
-const dburl = process.env.dburl;
+app.use(exp.static(path.join(__dirname,"./dist/ecommerceapp")));
+
+app.use(exp.json());
 
 
+const dburl=process.env.dburl
 
-const port = process.env.port;
-app.listen(port,()=>console.log(`Server started on port ${port}`))
+mongodb.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(
+    client => {
+        dbObj = client.db("MyAppDatabase");
+        userCollectionObj = dbObj.collection("usercollection");
+        productCollectionObj = dbObj.collection("productcollection");
+        cartCollectionObj = dbObj.collection("cartcollection");
+
+        app.set("userCollectionObj",userCollectionObj);
+        app.set("productCollectionObj",productCollectionObj);
+        app.set("cartCollectionObj",cartCollectionObj);
+        
+        console.log("connected to database");
+    }
+)
+.catch(error => console.log("error in db connection",error));
+
+app.use((req,res,next)=>{
+    res.send({message:req.url+" is invalid"});
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err.message);
+    res.send({message:"error occured",reason:err.message});
+})
+
+const port = process.env.PORT;
+
+app.listen(process.env.PORT,()=>console.log(`Server started on port ${port}`));
